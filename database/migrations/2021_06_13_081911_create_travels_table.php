6bdb38c146f810aad9cb32d25783e026364ae878
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-06-13 10:54:34 +0200

    first commit

---
+++ database/migrations/2021_06_13_081911_create_travels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 30);
+            $table->string('country', 20);
+            $table->float('price', 7, 2);
+            $table->tinyInteger('number_person');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
