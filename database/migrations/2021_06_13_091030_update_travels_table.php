56a2d1d5930cca3a50e5c04282a31cac5ce60c41
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-06-13 12:14:15 +0200

    controller e model creati - stampati dati db

---
+++ database/migrations/2021_06_13_091030_update_travels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->string('reservation_name', 20)->after('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->dropColumn('reservation_name');
+        });
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>ciao</h1>
+    <h1>Prenotazioni</h1>
+
+    <ul>
+        @foreach($travels as $travel)
+            <li>
+                <div>Destination: {{$travel->destination}}</div>
+                <div>Country: {{$travel->country}}</div>
+                <div>Reservation Name: {{$travel->reservation_name}}</div>
+                <div>Price: {{$travel->price}}</div>
+                <div>Guests: {{$travel->number_person}}</div>
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
